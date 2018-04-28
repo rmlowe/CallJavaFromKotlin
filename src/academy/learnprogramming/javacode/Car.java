faba1a8bec80516fa8627e8f423487d10b4620cd
Robert Lowe <robertmlowe@rmlowe.com>
2018-04-29 01:08:34 +0800

    Initial commit

---
+++ src/academy/learnprogramming/javacode/Car.java
@@ -0,0 +1,38 @@
+package academy.learnprogramming.javacode;
+
+public class Car {
+
+    private String color;
+    private String model;
+    private int year;
+
+    public Car(String color, String model, int year) {
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
