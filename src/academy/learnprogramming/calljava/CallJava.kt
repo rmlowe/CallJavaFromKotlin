26e12c92143b24a35763c84594b1fab9b3ff7bce
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-01 01:30:03 +0800

    More about calling Java from Kotlin

--- src/academy/learnprogramming/calljava/CallJava.kt
+++ src/academy/learnprogramming/calljava/CallJava.kt
@@ -9,11 +9,24 @@ fun main(args: Array<String>) {
     car.color = "green"
     println(car)
 
+    car.variableMethod(5, "hello", "goodbye")
+    val strings = arrayOf("hello", "goodbye")
+    car.variableMethod(10, *strings)
+
+    car.wantsIntArray(intArrayOf(1, 2, 3))
+
+    println("x = ${Car.x}")
+    println(Car.xString())
+
+    car.demoMethod( { println("I'm in a thread!") })
+
+    //(car.anObject as java.lang.Object).notify()
+
     //var model: String = car.model
     //println(model.javaClass)
     //model = null
     //println(model)
 
-    val color: Int = car.color
+    //val color: Int = car.color
 
 }
--- src/academy/learnprogramming/javacode/Car.java
+++ src/academy/learnprogramming/javacode/Car.java
@@ -4,14 +4,44 @@ import org.jetbrains.annotations.Nullable;
 
 public class Car {
 
+    public static int x = 5;
+
     private String color;
     private String model;
     private int year;
+    private Object anObject;
+
 
     public Car(String color, String model, int year) {
         this.color = color;
         this.model = model;
         this.year = year;
+    }
+
+    public void demoMethod(Runnable r) {
+
+        new Thread(r).start();
+
+        //new Thread( () -> System.out.println("I'm in a thread!") ).start();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.print("I'm in a thread!");
+//            }
+//        }).start();
+    }
+
+    public static String xString() {
+        return "This is x: " + x++;
+    }
+
+    public Object getAnObject() {
+        return anObject;
+    }
+
+    public void setAnObject(Object anObject) {
+        this.anObject = anObject;
     }
 
     public String getColor() {
@@ -36,6 +66,18 @@ public class Car {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public void variableMethod(int num, String... strings) {
+        for (String string: strings) {
+            System.out.println(string);
+        }
+    }
+
+    public void wantsIntArray(int[] theArray) {
+        for (int i: theArray) {
+            System.out.println("Here's the int: " + i);
+        }
     }
 
     @Override
